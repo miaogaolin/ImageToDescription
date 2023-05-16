f2df42f164b8fceed84a8ba35be8f4b3cf0b1249
miaogaolin <2825873215@qq.com>
2023-05-16 13:35:46 +0800

    test

--- main.py
+++ main.py
@@ -5,21 +5,17 @@ from clip_interrogator import Config, Interrogator
 import requests
 
 ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
-initImg = Image.open("./1.jpg").convert('RGB')
-ci.interrogate(initImg)
 
 app = Flask(__name__)
 
 @app.route('/')
 def ImageToDescription():
     url = request.args.get('url')
-    try:
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content)).convert('RGB')
-        print("res:",ci.interrogate(img))
-        return "done"
-    except Exception as e:   # 捕获所有Exception类及其子类抛出的异常/errors 
-        return e
+  
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+    return ci.interrogate_fast(img)
+   
 
 if __name__ == '__main__':
  app.run(debug=True, port=8083)
