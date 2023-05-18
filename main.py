e5a9de77ae59eb203662445b7c4540755e35d679
miaogaolin <2825873215@qq.com>
2023-05-18 14:47:04 +0800

    Add exception catching

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ from io import BytesIO
 from clip_interrogator import Config, Interrogator
 import requests
 
-ci = Interrogator(Config(clip_model_name="ViT-L-14/openai",chunk_size=10240))
+ci = Interrogator(Config(clip_model_name="ViT-L-14/openai",chunk_size=10240, caption_max_length=64))
 
 app = Flask(__name__)
 
 @app.route('/')
 def ImageToDescription():
-    url = request.args.get('url')
-    mode = request.args.get('mode')
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content)).convert('RGB')
-    
-    if mode == 'classic':
-       return ci.interrogate_classic(img)
-    elif mode == 'negative':
-       return ci.interrogate_negative(img)
-    elif mode == 'best':
-        return ci.interrogate(img)
-       
-    return ci.interrogate_fast(img)
+    try:
+        url = request.args.get('url')
+        mode = request.args.get('mode')
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content)).convert('RGB')
+        
+        if mode == 'classic':
+            return ci.interrogate_classic(img)
+        elif mode == 'negative':
+            return ci.interrogate_negative(img)
+        elif mode == 'best':
+            return ci.interrogate(img)
+        return ci.interrogate_fast(img)
+    except  Exception as e:
+       return str(e);
    
 
 if __name__ == '__main__':
