7108d1e55f1d6d08ae9380d5116cf31e5e49e012
miaogaolin <2825873215@qq.com>
2023-05-22 15:12:53 +0800

    增加oss批量处理

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PIL import Image
 from io import BytesIO
 from clip_interrogator import Config, Interrogator
 import requests
+import oss2
+import yaml
 
 ci = Interrogator(Config(clip_model_name="ViT-L-14/openai",chunk_size=10240, caption_max_length=64))
 
@@ -27,5 +29,29 @@ def ImageToDescription():
        return str(e);
    
 
+def GetOssImages(bucket, prefix=''):
+  for obj in oss2.ObjectIteratorV2(bucket,prefix=prefix):
+        pname=obj.key.replace(prefix,'',1).lstrip('/')
+        name=f"{prefix}/{pname}" if prefix else pname 
+        if obj.is_prefix():
+            GetOssImages(bucket,name)
+        elif name.endswith(('.jpg','.jpeg','.bmp','.gif','.png', '.webp')):
+            imgContent = bucket.get_object(name).read()
+            img = Image.open(BytesIO(imgContent)).convert('RGB')
+            
+            with open('image_des.csv', mode='a+', encoding='utf-8') as f:
+                des = ci.interrogate_fast(img)
+                f.write(name+','+des + '\n')
+   
 if __name__ == '__main__':
- app.run(debug=True, port=8083, host='0.0.0.0')
+    # app.run(debug=True, port=8083, host='0.0.0.0')
+    with open('conf.yaml', 'r') as f:
+        data = yaml.safe_load(f)
+        access_key_id = data['alioss']['accessKeyId']     # 替换为您的 access key id.
+        access_key_secret = data['alioss']['accessKeySecret']   # 替换为您的 access key secret.
+        # 对于公共访问，请设置endpoint：
+        bucket_name, endpoint = data['alioss']['bucket'],data['alioss']['endpoint']    # 填写自己在控制台上创建存储空间时指定的名字和地区域名。
+        auth = oss2.Auth(access_key_id, access_key_secret)
+        bucket = oss2.Bucket(auth, endpoint, bucket_name) 
+        GetOssImages(bucket)
+    
