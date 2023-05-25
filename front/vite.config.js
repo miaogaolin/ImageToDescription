a0d40e846efbc51f1f8f8fe4d9b4dd58e18edfa3
miaogaolin <2825873215@qq.com>
2023-05-25 11:43:10 +0800

    Update preview config

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		host: "0.0.0.0",
+		port: 8083
+	}
 });
